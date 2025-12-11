5121993830f461b3df7dbdabaf4ec8a7f2b192c1
jerkan.araujo <jerkan.araujo@pinapp.tech>
2025-12-11 13:44:53 -0300

    agrego start date y enddate

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get('/pedidos', (req, res) => {
   const status = req.query.status;
